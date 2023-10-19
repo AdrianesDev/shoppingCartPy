95a1626e33d18f7f28c719f89db2f8d749bbc025
stephany990517 <moronesstephany@gmail.com>
2023-10-18 20:26:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # Dinero inicial del usuario
 availableMoney = 100
-
 # Diccionario que contiene los productos y sus precios
 products = {
-    "Water": 10,
-    "Milk": 20,
-    "Eggs": 15
+    "1. Water": 10,
+    "2. Milk": 20,
+    "3. Eggs": 15,
+    "4. Other Products": 5,
+     
 }
 
-# Inicializamos el carrito como una lista vacía para almacenar los ítems.
+# Inicializamos el carrito como una lista vacía para almacenar los ítems
 cart = []
 
 # Función para mostrar el menú
@@ -18,7 +19,7 @@ def showMenu():
     print("2. View Cart")
     print("3. Add Item to Cart")
     print("4. Remove Item from Cart")
-    print("5. Purchase Items")
+    print("5. Total Amount")
     print("6. Exit")
 
 
@@ -35,7 +36,7 @@ def addItemToCart(product):
     cart.append(product)
     print(f"{product} has been added to your cart.")
 
-# Función para mostrar los ítems comprados.
+# Función para mostrar los ítems comprados
 def showPurchasedItems():
     if len(cart) == 0:
         print("Your cart is empty.")
@@ -43,13 +44,15 @@ def showPurchasedItems():
         print("Items in your cart:")
         for item in cart:
             print("- " + item)
+       
 
 # Función para comprar ítems en el carrito
+
 def purchaseItems():
     total = 0
     print("Items in your cart:")
     for item in cart:
-        print("- " + item)
+        print("- " + item) 
         # Calculamos el total sumando el precio de los productos
         if item == "Water":
             total += 10
@@ -57,23 +60,30 @@ def purchaseItems():
             total += 20
         elif item == "Eggs":
             total += 15
+ 
     print(f"Total amount: ${total}")
     # Limpiamos el carrito después de la compra
     cart.clear()
+
+
 
 def addProduct():
     if len(cart) >= 10:
         print("Sorry, you cannot add more than 10 products to the cart.")
     else:
         showProducts()
-        selection = input("Select a product (1-3): ")
+        selection = input("Select a product (1-4): ")
         product = None
         if selection == "1":
-            product = "Water"
+            product = " Water"
         elif selection == "2":
             product = "Milk"
         elif selection == "3":
             product = "Eggs"
+        elif selection == "4":
+            # Variable para que el usuario agrega cualquier otro producto 
+            other_products=input ("What do you want to add? ")
+            product =(f"""{other_products}""")
         else:
             print("Invalid option. Please select a valid number.")
 
@@ -118,8 +128,9 @@ while True:
         purchaseItems()
     elif option == "6":
         print("Thank you for using our shopping cart, see you later!")
+    
         break
     else:
-        print("Invalid option. Please select a number from 1 to 5.")
+        print("Invalid option. Please select a number from 1 to 6.")
 
 
